d3227aff286caf74b2d006cf694d71835d7fd44b
Glade <glade.pierre@outlook.fr>
2023-06-23 11:51:13 +0200

    Ajout next/previous sur les flèches

--- assets/script.js
+++ assets/script.js
@@ -1,80 +1,60 @@
 const slides = [
-	{
-		"image":"slide1.jpg",
-		"tagLine":"Impressions tous formats <span>en boutique et en ligne</span>"
-	},
-	{
-		"image":"slide2.jpg",
-		"tagLine":"Tirages haute définition grand format <span>pour vos bureaux et events</span>"
-	},
-	{
-		"image":"slide3.jpg",
-		"tagLine":"Grand choix de couleurs <span>de CMJN aux pantones</span>"
-	},
-	{
-		"image":"slide4.png",
-		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
-	}
+  {
+    image: "slide1.jpg",
+    tagLine: "Impressions tous formats <span>en boutique et en ligne</span>",
+  },
+  {
+    image: "slide2.jpg",
+    tagLine:
+      "Tirages haute définition grand format <span>pour vos bureaux et events</span>",
+  },
+  {
+    image: "slide3.jpg",
+    tagLine: "Grand choix de couleurs <span>de CMJN aux pantones</span>",
+  },
+  {
+    image: "slide4.png",
+    tagLine: "Autocollants <span>avec découpe laser sur mesure</span>",
+  },
 ];
 
-let arrow = document.querySelector('.arrow');
-let banner = document.querySelector('#banner');
-const arrow_left = document.querySelector('.arrow_left');
-const arrow_right = document.querySelector('.arrow_right');
+let arrow = document.querySelector(".arrow");
+let banner = document.querySelector("#banner");
+const arrow_left = document.querySelector(".arrow_left");
+const arrow_right = document.querySelector(".arrow_right");
 
+let current_slide = 0;
 
-arrow_left.addEventListener('click', function (ChangeSlide){ alert("Changement d'image vers la gauche");});
-arrow_right.addEventListener('click', function (ChangeSlide){ alert("Changement d'image vers la droite");});
+function previous_slide() {
+  current_slide--;
+  console.log(current_slide);
+  if (current_slide < 0) {
+    current_slide = slides.length - 1;
+  }
+  document.getElementById("slide").src =
+    "./assets/images/slideshow/" + slides[current_slide].image;
+}
+function next_slide() {
+  console.log("nextslide");
+  if (current_slide == 3) {
+    current_slide = 0;
+    document.getElementById("slide").src =
+      "./assets/images/slideshow/" + slides[current_slide].image;
+  } else {
+    document.getElementById("slide").src =
+      "./assets/images/slideshow/" + slides[current_slide + 1].image;
+  }
 
-let numero = 0;
+  current_slide++;
 
+  console.log(current_slide);
+}
 
-function ChangeSlide() {
-	document.getElementById("slide"). src = "./assets/images/slideshow/" + slides[1];
-	/*numero = numero + sens;
-    if (numero < 0)
+arrow_left.addEventListener("click", previous_slide);
+arrow_right.addEventListener("click", next_slide);
+
+/*numero = numero + sens;
+    if (current < 0)
         numero = slides.length - 1;
     if (numero > slides.length - 1)
         numero = 0;*/
-};
-
-function prev() {
-	ChangeSlide( - 1);
-  }
-
-function next() {
-	ChangeSlide( + 1);
-  }
-
-ChangeSlide();
-next;
-prev;
-
-
-
-
-
-console.log(slides[numero])
-
-
-
-
-
-/*arrow.addEventListener('click', (event) => {
-	if(event.button == 0) {
-		{alert ("Left click detected")}
-		console.log("Left click detected")
-	}else{
-		{alert ("Right click detected")};
-	}
-});*/
-
-
-
-
-
-/*arrow.addEventListener('click', function(){ alert("Left click detected")});*/
-
-
-
-
