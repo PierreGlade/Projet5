53c82eeea6cecf50d1c1a0399ddbe6296a6b5a2d
Glade <glade.pierre@outlook.fr>
2023-06-28 10:27:25 +0200

    Essais bullet point actif

--- assets/script.js
+++ assets/script.js
@@ -25,17 +25,33 @@ const arrow_right = document.querySelector(".arrow_right");
 
 let current_slide = 0;
 
-function previous_slide() {
+/* Essai 1 *//*
+let div = document.querySelector('#current_dot');
+for (let cssClass of div.classList) {
+    console.log(cssClass);
+    div.className += " dot_selected";
+}*/
+
+/* Essai 2 *//*
+current_slide.classList.add('dot_selected');
+*/
+
+/* Essai 3 *//*
+document.getElementById(current_slide).classList.add("dot_selected");
+*/
+
+
+function previous_slide() {	
   current_slide--;
   console.log(current_slide);
   if (current_slide < 0) {
     current_slide = slides.length - 1;
   }
   document.getElementById("slide").src =
-    "./assets/images/slideshow/" + slides[current_slide].image;
+    "./assets/images/slideshow/" + slides[current_slide].image;	
 }
+
 function next_slide() {
-  console.log("nextslide");
   if (current_slide == 3) {
     current_slide = 0;
     document.getElementById("slide").src =
@@ -44,17 +60,12 @@ function next_slide() {
     document.getElementById("slide").src =
       "./assets/images/slideshow/" + slides[current_slide + 1].image;
   }
-
   current_slide++;
-
   console.log(current_slide);
 }
+
 
 arrow_left.addEventListener("click", previous_slide);
 arrow_right.addEventListener("click", next_slide);
 
-/*numero = numero + sens;
-    if (current < 0)
-        numero = slides.length - 1;
-    if (numero > slides.length - 1)
-        numero = 0;*/
+
